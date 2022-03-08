7c375064941fbe387c10486ac2ba61155146249c
gorkemcirtma <210704312@st.maltepe.edu.tr>
2022-03-08 13:24:09 +0300

    initial comit

---
+++ tr.maltepe.edu.oop/main.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+public class main {
+    public static void main(String[] args) {
+        prof prof1=new prof("gorkem",21);
+        prof prof2=new prof("gorkem",22);
+        prof prof3=new prof("gorkem",23);
+        student student1=new student("gizem",23);
+        student student2=new student("gizem",24);
+        student student3=new student("gizem",25);
+        System.out.println(prof1.detayGoster());
+        System.out.println(prof2.detayGoster());
+        System.out.println(prof3.detayGoster());
+        System.out.println(student1.detayGoster());
+        System.out.println(student2.detayGoster());
+        System.out.println(student3.detayGoster());
+
+
+
+
+
+
+
+    }
+}
---
+++ tr.maltepe.edu.oop/prof.java
@@ -0,0 +1,12 @@
+public class prof {
+    private String name;
+    private int age;
+    public prof(String name,int age){
+        this.name=name;
+        this.age=age;
+
+    }
+    public String detayGoster(){
+        return this.name+"  " +this.age;
+    }
+}
